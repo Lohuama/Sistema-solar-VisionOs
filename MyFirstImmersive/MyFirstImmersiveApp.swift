04eb693a6d708341fc567dac913860158a625ed0
Lohuama <limalorruama98@gmail.com>
2026-06-27 18:11:53 -0300

    Initial Commit

---
+++ MyFirstImmersive/MyFirstImmersiveApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyFirstImmersiveApp.swift
+//  MyFirstImmersive
+//
+//  Created by Lohuama Lima on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstImmersiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
